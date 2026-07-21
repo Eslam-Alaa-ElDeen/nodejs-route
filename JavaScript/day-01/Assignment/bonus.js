6da90fd9fe84d2f3cd1f9f83835d669a6bc5539a
Eslam Alaa <ea33669900@gmail.com>
2026-07-21 05:28:21 +0300

    add bonus and essay for assignment one lec one

---
+++ JavaScript/day-01/Assignment/bonus.js
@@ -0,0 +1,28 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+
+
+var createCounter = function(init) {
+    const num=init;
+    return counter={
+        increment:()=>{
+            return ++init;
+        },
+        reset:()=>{
+            init=num;
+            return init
+        },
+        decrement:()=>{
+            return --init;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
