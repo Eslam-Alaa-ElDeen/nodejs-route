955dd3a0ec341aff1ca3082d08f167ee16cf4f1f
Eslam Alaa <ea33669900@gmail.com>
2026-08-04 15:33:12 +0300

    last update on stream

--- NodeJS/day-01/drive/streams/stream.js
+++ NodeJS/day-01/drive/streams/stream.js
@@ -1,21 +1,88 @@
 const fs=require("node:fs")
 const path=require("node:path")
+const {createGzip}=require("node:zlib")
+const zip=createGzip();
 
-const source=path.resolve("./data.txt")
-const dest=path.resolve("./dest.txt")
+const source=path.resolve("./newData.txt")
+const dest=path.resolve("./newDest.txt")
+const destZip=path.resolve("./newDest.txt.gz")
+
 
 //=================================================================================================================
 //=====read stream and write stream
 //=================================================================================================================
-console.log({source});
+// stream.on("eventName", callback);
 
-const readStream=fs.createReadStream(source,{highWaterMark:300})
+// console.log({source});
+
+const readStream=fs.createReadStream(source,{highWaterMark:300,encoding:"utf-8"})
 const writeStream=fs.createWriteStream(dest);
 
-readStream.on("data",(chunk)=>{
-    console.log("=================================================================================================================");
-    console.log(chunk);
-    console.log("=================================================================================================================");
-    writeStream.write(chunk)
+const writeZipStream=fs.createWriteStream(destZip);
 
-})
+
+// readStream.on("data",(chunk)=>{
+//     console.log("=================================================================================================================");
+//     console.log(chunk);
+//     console.log("=================================================================================================================");
+//     writeStream.write(chunk)
+
+// })
+
+// ==========================
+// end
+// // ==========================
+// readStream.on("end", () => {
+
+// });
+
+
+// // ==========================
+// // error
+// // ==========================
+// readStream.on("error", (err) => {
+
+// });
+
+
+// // ==========================
+// // open
+// // ==========================
+// readStream.on("open", (fd) => {
+
+// });
+
+
+// // ==========================
+// // close
+// // ==========================
+// readStream.on("close", () => {
+
+// });
+
+
+// // ==========================
+// // readable
+// // ==========================
+// readStream.on("readable", () => {
+
+// });
+
+
+// // ==========================
+// // pause
+// // ==========================
+// readStream.on("pause", () => {
+
+// });
+
+
+// // ==========================
+// // resume
+// // ==========================
+// readStream.on("resume", () => {
+
+// });
+
+readStream.pipe(writeStream);
+readStream.pipe(zip).pipe(writeStream);
